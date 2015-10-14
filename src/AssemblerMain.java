0a508e991ed7aa2254618d8f4df2aff132574191
Jim <gilderjw@rose-hulman.edu>
2015-10-14 15:24:30 -0400

    changes all assembly into the respective machine code values. TODO: implement labels

--- src/AssemblerMain.java
+++ src/AssemblerMain.java
@@ -2,26 +2,104 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 public class AssemblerMain {
+	public static HashMap<String, String> opcodes = new HashMap<String, String>();
+	public static HashMap<String, String> registers = new HashMap<String, String>();
 	
 	public static String assemble(String code){
 		
-		int lineNumber = 0xf000;
+		int codeLineNumber = 0; //assembly line number
+		int lineNumber = 0xf000; //machine code address
 		String machineCode = "";
 		
 		String[] lines = code.split("\n");
 		
+		//Scan through code to find labels
 		for(String line: lines){
-			machineCode += String.format("%H",lineNumber) + " " + line.replace("li", "7") + "\n";
+			//TODO: implement 
+		}
+		
+		//actually assemble the code
+		for(String line: lines){
+			codeLineNumber++;
+			if(line.hashCode() == 0)
+				continue; 
+			
+			String[] instr = line.split(" |\\\t");//seperate parts
+			
+			machineCode += String.format("0x%H",lineNumber) + " ";
+			machineCode += opcodes.get(instr[0]);
+			
+			if(!instr[0].equals("li")){
+				for(int i = 1; i < 4; i++){ //each hex digit
+					if(instr.length > i){
+						if(registers.get(instr[i]) == null)
+							System.err.println("Error near line " + codeLineNumber);
+						
+						machineCode += registers.get(instr[i]);
+					} else{
+						machineCode += "Q";
+					}
+				}
+			} else{
+				try{
+					if(Integer.parseInt(instr[1]) > 0x0FFF){
+						System.err.println("Error near line " + codeLineNumber);
+					}
+				} catch (Exception e){
+					System.err.println("Error near line " + codeLineNumber);
+				}
+				machineCode += String.format("%H3", instr[1]);
+			}
+			machineCode += "\n";
 			lineNumber += 2;
 		}
 		
 		return machineCode;
 		
 	}
+	
+	public static void setUpMaps(){
+		opcodes.put("lw", "0");
+		opcodes.put("sw", "1");
+		opcodes.put("add", "2");
+		opcodes.put("j", "3");
+		opcodes.put("beq", "4");
+		opcodes.put("sll", "5");
+		opcodes.put("srl", "6");
+		opcodes.put("li", "7");
+		opcodes.put("and", "8");
+		opcodes.put("or", "9");
+		opcodes.put("ret", "A");
+		opcodes.put("sub", "B");
+		opcodes.put("jal", "C");
+		opcodes.put("push", "D");
+		opcodes.put("pop", "E");
+		opcodes.put("mov", "F");
+		
+		registers.put("$imm", "0");
+		registers.put("$ret", "1");
+		registers.put("$at", "2");
+		registers.put("$p0", "3");
+		registers.put("$p1", "4");
+		registers.put("$t0", "5");
+		registers.put("$t1", "6");
+		registers.put("$t2", "7");
+		registers.put("$t3", "8");
+		registers.put("$t4", "9");
+		registers.put("$t5", "A");
+		registers.put("$t6", "B");
+		registers.put("$t7", "C");
+		registers.put("$ra", "D");
+		registers.put("$s0", "E");
+		registers.put("$s1", "F");
+	}
 	public static void main(String[] args) {
+		setUpMaps();
+		
 		if(args.length != 2){
 			System.err.println("invalid number of parameters \"input, output\"");
 			return;
@@ -50,7 +128,7 @@ public class AssemblerMain {
 			System.err.println("something wrong");
 			return;
 		}
-		
+	
 		System.out.println(assemble(code));
 	}
 }
